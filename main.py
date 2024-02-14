c5834029dc048d9cc1a565a7dfa923fa2fe5dcf6
Stanislav <stanislav.yas@gmail.com>
2024-02-14 22:31:19 +0600

    Ch.2.6

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import requests
+from json.decoder import JSONDecodeError
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a JSON format")
+    
