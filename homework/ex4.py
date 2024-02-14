9046749d3eb795b93e3404ca0b120cf74f129eae
Stanislav <stanislav.yas@gmail.com>
2024-02-14 22:08:07 +0600

    Ch.2.4

--- homework/ex4.py
+++ homework/ex4.py
@@ -1,4 +1,5 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text) # {"answer":"Hello, someone"}
+payload = {"name": "User"}
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+print(response.text) # {"answer":"Hello, User"}
