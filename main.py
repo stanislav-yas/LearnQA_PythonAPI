57ede4d0f6089fb382d6a060abe8643a6877affe
Stanislav <stanislav.yas@gmail.com>
2024-02-16 19:47:01 +0600

    Ch.2.8

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
 
-params = {"name": "Stas"}
-response = requests.get("https://playground.learnqa.ru/api/check_type", params=params)
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/get_500")
+print(response.status_code)
 
-response = requests.put("https://playground.learnqa.ru/api/check_type", data=params)
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/something")
+print(response.status_code)
+
+response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+
+if len(response.history) > 0:
+    print(response.history[0].status_code)
+    print(response.history[0].url); 
+
+print(response.status_code)
+print(response.url); 
