702362435d9e55fa0d64bf6ede8f4ca33d6e0f05
Stanislav <stanislav.yas@gmail.com>
2024-02-16 19:48:51 +0600

    Ch.2.9

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_500")
-print(response.status_code)
-
-response = requests.post("https://playground.learnqa.ru/api/something")
-print(response.status_code)
-
-response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
-
-if len(response.history) > 0:
-    print(response.history[0].status_code)
-    print(response.history[0].url); 
-
-print(response.status_code)
-print(response.url); 
+headers = {"my_header": "my_header_value"}
+response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+print(f"Request headers: {response.text}")
+print(f"Response headers: {response.headers}")
