f216557a2fa0302009102ead02d7cee0f2612120
Stanislav <stanislav.yas@gmail.com>
2024-02-23 16:07:55 +0600

    Ch.3.3

--- test_first_api.py
+++ test_first_api.py
@@ -1,9 +1,17 @@
+import pytest
 import requests
 
 class TestFirstAPI:
-    def test_hello_call(self):
+
+    names = [
+        ("John"),
+        ("Станислав"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Stanislav'
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -13,6 +21,9 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, f"Actual text in the response is not correct: {actual_response_text}"
