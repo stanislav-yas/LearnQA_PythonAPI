22b02c2994d9f35bc2a72af353b2aef5a28e7648
Stanislav <stanislav.yas@gmail.com>
2024-03-06 17:23:25 +0600

    #save on 06.03.24

--- test/test_user_auth.py
+++ test/test_user_auth.py
@@ -24,6 +24,7 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")  
 
+    @allure.title("User authorization")
     @allure.description("This test successfully authorize user by email and password")
     def test_auth_user(self):
 
@@ -44,8 +45,9 @@ class TestUserAuth(BaseCase):
             "User id from auth method is not equal to user id from check method"
         )
 
-    @allure.description("This test checks authorization status w/o sending auth cookies or token")
     @pytest.mark.parametrize('condition', conditions)
+    @allure.title("User authorization with condition: {condition}")
+    @allure.description("This test checks authorization status w/o sending auth cookies or token")    
     def test_negative_auth_check(self, condition):
 
         if condition == "no_cookie":
--- test/test_user_delete.py
+++ test/test_user_delete.py
@@ -18,103 +18,108 @@ class TestUserDelete(BaseCase):
         self.user_password = user_data['password']
         self.user_id = self.get_json_value(response1, "id")
 
-    @allure.description("Попытка удалить пользователя с ID 2")
+    @allure.title("Attempt to delete user with id = 2")
     def test_delete_user_id_2(self):
-        # Login as user ID 2
-        response1 = MyRequests.post(
-            "/user/login", 
-            data = {
-                'email': 'vinkotov@example.com',
-                'password': '1234'
-            }
-        )
-        Assertions.assert_status_code(response1, 200)
 
-        auth_sid = self.get_cookie(response1, "auth_sid")
-        token = self.get_header(response1, "x-csrf-token")
+        with allure.step("Login as user with ID=2"):
+            response1 = MyRequests.post(
+                "/user/login", 
+                data = {
+                    'email': 'vinkotov@example.com',
+                    'password': '1234'
+                }
+            )
+            Assertions.assert_status_code(response1, 200)
 
-        # Try to delete user with ID=2     
-        user2_id = 2
+            auth_sid = self.get_cookie(response1, "auth_sid")
+            token = self.get_header(response1, "x-csrf-token")
 
-        response2 = MyRequests.delete(
-            f"/user/{user2_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
-        )
+        with allure.step("Attempt to delete the user with ID=2"):
+            response2 = MyRequests.delete(
+                f"/user/2",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid}
+            )
 
-        Assertions.assert_status_code(response2, 400)
-        assert response2.text == 'Please, do not delete test users with ID 1, 2, 3, 4 or 5.', \
-            f"Unexpected response content: {response2.text}"
+            Assertions.assert_status_code(response2, 400)
+            assert response2.text == 'Please, do not delete test users with ID 1, 2, 3, 4 or 5.', \
+                f"Unexpected response content: {response2.text}"
 
+    @allure.title("Authorized user deletion")
     @allure.description("Позитивный тест на удаление пользователя")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_delete_user_auth(self):
-        # Login as a new user (created in setup)
-        response1 = MyRequests.post(
-            "/user/login", 
-            data = {
-                'email': self.user_email,
-                'password': self.user_password
-            }
-        )
 
-        Assertions.assert_status_code(response1, 200)
+        with allure.step("Login as a new user (created in setup)"):
+            response1 = MyRequests.post(
+                "/user/login", 
+                data = {
+                    'email': self.user_email,
+                    'password': self.user_password
+                }
+            )
 
-        auth_sid = self.get_cookie(response1, "auth_sid")
-        token = self.get_header(response1, "x-csrf-token")
+            Assertions.assert_status_code(response1, 200)
 
-        # Try to delete a new user   
-        response2 = MyRequests.delete(
-            f"/user/{self.user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
-        )
+            auth_sid = self.get_cookie(response1, "auth_sid")
+            token = self.get_header(response1, "x-csrf-token")
 
-        Assertions.assert_status_code(response2, 200)
+        with allure.step("Attempt to delete a new user"):
+            response2 = MyRequests.delete(
+                f"/user/{self.user_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid}
+            )
 
-        # Checking a new user deletion
-        response3 = MyRequests.get(
-            f"/user/{self.user_id}"
-        )
+            Assertions.assert_status_code(response2, 200)
 
-        Assertions.assert_status_code(response3, 404)
-        assert response3.text == 'User not found', f"Unexpected response content: {response3.text}"
+        with allure.step("Checking the new user deletion"):
+            response3 = MyRequests.get(
+                f"/user/{self.user_id}"
+            )
 
+            Assertions.assert_status_code(response3, 404)
+            assert response3.text == 'User not found', f"Unexpected response content: {response3.text}"
+
+    @allure.title("Another user deletion")
     @allure.description("Попробовать удалить пользователя, будучи авторизованными другим пользователем")
+    @allure.label("Negative")
     def test_delete_user_auth_another_user(self):
-        # Login as user #1 (created in setup)
-        response1 = MyRequests.post(
-            "/user/login", 
-            data = {
-                'email': self.user_email,
-                'password': self.user_password
-            }
-        )
-        Assertions.assert_status_code(response1, 200)
 
-        auth_sid = self.get_cookie(response1, "auth_sid")
-        token = self.get_header(response1, "x-csrf-token")    
+        with allure.step("Login as user#1 (created in setup)"):
+            response1 = MyRequests.post(
+                "/user/login", 
+                data = {
+                    'email': self.user_email,
+                    'password': self.user_password
+                }
+            )
+            Assertions.assert_status_code(response1, 200)
 
-        # New user #2 registration
-        user2_data = self.prepare_registation_data()
-        response2 = MyRequests.post("/user/", data=user2_data)
+            auth_sid = self.get_cookie(response1, "auth_sid")
+            token = self.get_header(response1, "x-csrf-token")    
 
-        Assertions.assert_status_code(response2, 200)
-        Assertions.assert_json_has_key(response2, 'id')
-        user2_id = self.get_json_value(response2, 'id')
+        with allure.step("New user #2 registration"):
+            user2_data = self.prepare_registation_data()
+            response2 = MyRequests.post("/user/", data=user2_data)
 
-        # Try to delete another user (just created user #2)     
-        response3 = MyRequests.delete(
-            f"/user/{user2_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
-        )
+            Assertions.assert_status_code(response2, 200)
+            Assertions.assert_json_has_key(response2, 'id')
+            user2_id = self.get_json_value(response2, 'id')
 
-        Assertions.assert_status_code(response3, 200)
+        with allure.step("Attempt to delete another user (just created user#2)"):
+            response3 = MyRequests.delete(
+                f"/user/{user2_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid}
+            )
 
-        # Checking the user #2 are not deleted
-        response4 = MyRequests.get(
-            f"/user/{user2_id}"
-        )
+            Assertions.assert_status_code(response3, 200)
 
-        Assertions.assert_status_code(response4, 200)
-        Assertions.assert_json_has_key(response4, 'username')
+        with allure.step("Checking the user#2 are not deleted"):
+            response4 = MyRequests.get(
+                f"/user/{user2_id}"
+            )
+
+            Assertions.assert_status_code(response4, 200)
+            Assertions.assert_json_has_key(response4, 'username')
